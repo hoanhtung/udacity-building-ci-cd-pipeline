aa71ce6a8d2be85a53301d474459e033af97b4b4
Hoanh Tung <hoanhtung.xy@gmail.com>
2022-07-03 03:15:04 +0000

    add locust file

---
+++ locustfile.py
@@ -0,0 +1,14 @@
+
+import time
+from locust import HttpUser, task, TaskSet, between
+
+class MTaskSet(HttpUser):
+    wait_time = between(1, 3)
+
+    @task
+    def home(self):
+        self.client.get("/")
+
+    @task(3)
+    def predict(self):
+        self.client.post("/predict", json={"CHAS" :{"0" :0}, "RM" :{"0" :6.575}, "TAX" :{"0" :296.0}, "PTRATIO" :{"0" :15.3}, "B" :{"0" :396.9}, "LSTAT" :{"0" :4.98}})
